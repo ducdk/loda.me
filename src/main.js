c46ec368dac580e83e59bd69eea9db160a65eec5
Nguyen Hoang Nam <nguyen.hoang.nam@linecorp.com>
2019-04-11 18:01:31 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,44 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+// CSS
+import "~/assets/css/dashboard.css";
+
+import DefaultLayout from "~/layouts/Default.vue";
+
+export default function(Vue, { router, head, isClient }) {
+  head.link.push(
+    {
+      rel: "stylesheet",
+      href: "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext"
+    },
+    {
+      rel: "stylesheet",
+      href: "/assets/css/dashboard.css"
+    },
+    {
+      rel: "stylesheet",
+      href: "/assets/plugins/charts-c3/plugin.css"
+    },
+    {
+      rel: "stylesheet",
+      href: "/assets/plugins/maps-google/plugin.css"
+    }
+  );
+  head.script.push({ src: "/assets/js/require.min.js" }, {});
+  head.script.push({ innerHTML: "requirejs.config({baseUrl: '.'})", type: "text/javascript" });
+  head.script.push(
+    { src: "/assets/js/dashboard.js" },
+    { src: "/assets/plugins/charts-c3/plugin.js" },
+    { src: "/assets/plugins/maps-google/plugin.js" },
+    { src: "/assets/plugins/input-mask/plugin.js" },
+    { src: "/assets/plugins/datatables/plugin.js" }
+  );
+
+  // Set default layout as a global component
+  Vue.component("Layout", DefaultLayout);
+}
