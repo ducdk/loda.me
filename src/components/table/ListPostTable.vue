15210cf3f8f511e2216cff86484b4c4caf88f9da
Nguyen Hoang Nam <nguyen.hoang.nam@linecorp.com>
2019-04-19 18:01:48 +0700

    update index

---
+++ src/components/table/ListPostTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="table-responsive">
+    <table class="table card-table table-vcenter">
+      <!-- <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Date</th>
+                  </tr>
+      </thead>-->
+      <tbody>
+        <tr v-for="edge in articles.edges.slice(0,7)" :key="edge.node.id">
+          <td>
+            <g-link :to="edge.node.path">{{edge.node.title}}</g-link>
+          </td>
+          <td v-if="showDate" class="text-nowrap w-1">May 6, 2018</td>
+          <!-- <td class="w-1">
+                      <a href="#" class="icon">
+                        <i class="fe fe-trash"></i>
+                      </a>
+          </td>-->
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: 'ListPostTable',
+    props: {
+        articles: Object,
+        showDate: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,32 +2,31 @@
   <Default title="Home">
     <div class="mb-6">
       <!-- top -->
-      <div class="row row-cards row-deck">
+      <div class="row row-cards">
         <div class="col-lg-4 col-md-6">
-          <Card :hideTitle="true" :bodyClass="{'p-0': true}">
+          <Card borderless :hideTitle="true" :bodyClass="{'p-0': true}">
             <!-- <g-image :src="$page.articles.edges[0].node.image"/> -->
-            <Carousel :edges="$page.articles.edges.slice(0,4)"/>
+            <Carousel :edges="$page.articles.edges.slice(0,7)"/>
+          </Card>
+          <Card borderless color="bg-orange" title="Xem nhiều" bodyClass="p-0">
+            <ListPostTable :showDate="false" :articles="$page.articles"/>
           </Card>
         </div>
 
         <div class="col-lg-8 col-md-6">
-          <Card borderless color="bg-green" title="Mới cập nhật">
-            <div
-              v-for="(edge, i) in $page.articles.edges.slice(0,7)"
-              :key="edge.node.id"
-              :ref="'card' + i"
-            >
-              <p>{{edge.node.title}}</p>
-            </div>
+          <Card borderless color="bg-green" title="Mới cập nhật" bodyClass="p-0">
+            <ListPostTable :articles="$page.articles"/>
+            
           </Card>
+          <EmailCard/>
         </div>
       </div>
 
       <!-- middle -->
-      <div class="row row-cards row-deck">
+      <div class="row row-cards">
         <!-- hot news -->
         <div class="col-lg-4">
-          <Card borderless color="bg-orange" title="Xem nhiều">
+          <!-- <Card borderless color="bg-orange" title="Xem nhiều">
             <div
               v-for="(edge, i) in $page.articles.edges.slice(0,7)"
               :key="edge.node.id"
@@ -35,14 +34,14 @@
             >
               <p>{{edge.node.title}}</p>
             </div>
-          </Card>
+          </Card>-->
         </div>
         <div class="col-lg-8">
           <!-- <div>
             <InlineTagList/>
-          </div> -->
+          </div>-->
           <div>
-            <EmailCard/>
+            <!-- <EmailCard/> -->
           </div>
         </div>
       </div>
@@ -66,6 +65,7 @@
 
 <script>
 import Card from "~/components/cards/Card.vue";
+import ListPostTable from "~/components/table/ListPostTable.vue";
 import Carousel from "~/components/cards/Carousel.vue";
 import InlineList from "~/components/cards/InlineList.vue";
 import EmailCard from "~/components/cards/EmailCard.vue";
@@ -77,6 +77,7 @@ export default {
   },
   components: {
     Card,
+    ListPostTable,
     Carousel,
     InlineList,
     EmailCard,
