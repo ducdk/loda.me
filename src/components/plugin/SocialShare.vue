390aaf76933714ede75e905e7f177dfdffc1d1a4
loda <loda.namnh@gmail.com>
2019-04-21 10:44:37 +0700

    update SocialShare

--- src/components/plugin/SocialShare.vue
+++ src/components/plugin/SocialShare.vue
@@ -1,15 +1,30 @@
 <template>
   <div :class="{'sticky-top' : sticky}">
     <div class="h6">Share</div>
-    <button type="button" class="btn btn-icon btn-facebook mr-1">
+    <a
+      class="btn btn-icon btn-facebook mr-1"
+      target="_blank"
+      :href="facebook"
+      onclick="window.open(this.href, 'facebook-share', 'width=550,height=435');return false;"
+    >
       <i class="fab fa-facebook-f"></i>
-    </button>
-    <button type="button" class="btn btn-icon btn-twitter mr-1">
+    </a>
+    <a
+      class="btn btn-icon btn-twitter mr-1"
+      target="_blank"
+      :href="twitter"
+      onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;"
+    >
       <i class="fab fa-twitter"></i>
-    </button>
-    <button type="button" class="btn btn-icon btn-google mr-1">
+    </a>
+    <a
+      class="btn btn-icon btn-google mr-1"
+      target="_blank"
+      :href="google"
+      onclick="window.open(this.href, 'facebook-google', 'width=550,height=435');return false;"
+    >
       <i class="fab fa-google"></i>
-    </button>
+    </a>
     <button type="button" class="btn btn-icon btn-github mr-1">
       <i class="fab fa-github"></i>
     </button>
@@ -18,10 +33,44 @@
 
 <script>
 export default {
-    name: 'SocialShare',
-    props:{
-        sticky: Boolean
+  name: "SocialShare",
+  props: {
+    sticky: Boolean,
+    title: String,
+    href: String
+  },
+  computed: {
+    facebook() {
+      return (
+        "https://facebook.com/sharer.php?u=" +
+        this.$static.metaData.siteUrl +
+        this.href
+      );
+    },
+    twitter() {
+      return (
+        "https://twitter.com/intent/tweet?text=" +
+        this.title +
+        "&url=" +
+        this.$static.metaData.siteUrl +
+        this.href
+      );
+    },
+    google() {
+      return (
+        "https://plus.google.com/share?url=" +
+        this.$static.metaData.siteUrl +
+        this.href
+      );
     }
-}
+  }
+};
 </script>
 
+<static-query>
+query {
+  metaData {
+    siteUrl
+  }
+}
+</static-query>
